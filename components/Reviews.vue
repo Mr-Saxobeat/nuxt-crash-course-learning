6088f08591153b84e3fc075263f8ed23b30c4928
Weiglas <weiglasribeiro@gmail.com>
2021-04-11 12:28:55 -0300

    Cria componente Review

---
+++ components/Reviews.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h3>Customer Reviews</h3>
+        <div v-if="!$fetchState.pending">
+            <ReviewCard
+                v-for="rev in reviewers.results"
+                :key="rev.login.uuid"
+                :review="rev"
+            />
+        </div>
+        <div v-else>
+            Loading...
+        </div>
+    </div>
+</template>
+
+<script>
+import ReviewCard from './ReviewCard.vue'
+    export default {
+        data() {
+            return {
+                reviewers: [],
+            }
+        },
+        async fetch() {
+            this.reviewers = await fetch(
+                "https://randomuser.me/api/?results=5"
+            ).then(res => res.json())
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
